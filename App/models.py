9d301d30dd1f07633e957912dd1a158030178dce
unknown <SENA@MOSAPRCBAFSD060.FormacionSena.RED>
2021-10-15 17:34:13 -0500

    se creo el modelo

--- App/models.py
+++ App/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    Nombre=models.CharField(max_length=50)
+    Apellido=models.CharField(max_length=50)
+    Contraseña=models.CharField(max_length=50)
